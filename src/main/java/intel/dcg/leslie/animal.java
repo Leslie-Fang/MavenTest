e7b4f17c4f8049dcd517a6990b4355fae2fe82b8
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-01-30 20:00:09 +0800

    add animal interface

---
+++ src/main/java/intel/dcg/leslie/animal.java
@@ -0,0 +1,9 @@
+package intel.dcg.leslie;
+//接口没有构造函数，没有实例变量和静态变量，但可以有 static final
+//抽象类可以有构造函数
+
+public interface animal{
+    public void sayName();
+    public void sayNumber();
+    public boolean eat(String food);
+}
